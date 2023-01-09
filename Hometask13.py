10320f0c1ab0b90af3544bcf592b3d7f75d4cd86
innateterina <innacharykova@gmail.com>
2023-01-09 20:49:29 +0000

    task inside

--- Hometask13.py
+++ Hometask13.py
@@ -1,50 +1,15 @@
-def func_2():
-    answer = input('Ваше нечетное число больше 2? (Д/H): ')
-    if answer.upper() in ('Д', 'д'):
-        answer_1 = input('Ваше число больше 4? (Д/H): ')
-
-    elif answer.upper() in ('Н', 'н'):
-        print('Вы загадали число 1')
-
-    if answer_1.upper() in ('Д', 'д'):
-        answer_2 = input('Ваше число больше 6? (Д/H): ')
-
-    elif answer_1.upper() in ('Н', 'н'):
-        print('Вы загадали число 3')
-
-    if answer_2.upper() in ('Д', 'д'):
-        answer_3 = input('Ваше число больше 8? (Д/H): ')
-
-    elif answer_2.upper() in ('Н', 'н'):
-        print('Вы загадали число 5')
-
-    if answer_3.upper() in ('Д', 'д'):
-        print('Вы загадали число 9')
-    else:
-        print('Вы загадали число 7')
-
+#Сделать программу в виде функций в которой нужно будет угадывать число.
+def number():
+    for i in range(1, 10 + 1):
+        i += 1
+        a = int(input('Угадайте целое число от 1 до 10: '))
+        while a != i:
+            if a < i:
+                    print('Ваше число меньше, чем задумал компьютер')
+            elif a > i:
+                    print('Ваше число больше, чем задумал компьютер')
+            a = int(input('Повторите попытку '))
+        print('Вы угадали')
     return
 
-def func_1():
-    print(input('Загадайте число от 1 до 9 и нажмите Enter:'))
-    answer = input('Ваше число четное? (Д/H): ')
-    if answer.upper() in ('Д', 'д'):
-        answer_1 = input('Ваше число больше 3? (Д/H): ')
-    elif answer.upper() in ('н', 'Н'):
-        print(func_2())
-    if answer_1.upper() in ('Д', 'д'):
-        answer_2 = input('Ваше число больше 5? (Д/H): ')
-    elif answer_1.upper() in ('Н', 'н'):
-        print('Вы загадали число 2')
-    if answer_2.upper() in ('Д', 'д'):
-        answer_3 = input('Ваше число больше 7? (Д/H): ')
-    elif answer_2.upper() in ('Н', 'н'):
-        print('Вы загадали число 4')
-    if answer_3.upper() in ('Д', 'д'):
-        print('Вы загадали число 8')
-    elif answer_3.upper() in ('Н', 'н'):
-        print('Вы загадали число 6')
-
-    return
-
-print(func_1())
+number()
