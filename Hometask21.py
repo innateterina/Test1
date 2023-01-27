43eca26f9b4d6e712236ffc6c76da72f9b258d61
innateterina <innacharykova@gmail.com>
2023-01-27 12:37:25 +0000

    task inside

---
+++ Hometask21.py
@@ -0,0 +1,27 @@
+#Создать родительский класс auto, у которого есть атрибуты:
+#brand, age, cоlor, mark и weight.
+#А так же методы: move, birthday и stop.
+#Методы move и stop выводят сообщение на экран «move» и «stop»,а birthday увеличивает атрибут age на 1.
+#Атрибуты brand, age и mark являются обязательными при объявлении объекта.
+
+class Auto:
+    brand = 'Ford'
+    age = 5
+    mark = 'Focus'
+    color = 'black'
+    weight = 5.5
+
+    def __int__(self, brand='Ford', age=5, mark='Focus'):
+        self.brand = brand
+        self.age = age
+        self.mark = mark
+
+    def move(self):
+        print("move")
+
+    def stop(self):
+        print("stop")
+
+    def birthday(self):
+        age += 1
+
