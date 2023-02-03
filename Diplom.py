a5f4c94936f460efac02f689ff6e5b97e6cb2a8b
innateterina <innacharykova@gmail.com>
2023-02-03 11:21:46 +0000

    task inside

---
+++ Diplom.py
@@ -0,0 +1,15 @@
+while True:
+    a = input("Введите имя: ")
+    b = input("Введите фамилию: ")
+    c = input("Введите отчество: ")
+    d = input("Введите дату рождения в формате <д/м/год>: ")
+    e = input("Введите дату смерти<д/м/год>, если человек жив - нажмите пробел: ")
+    f = input("Введите пол человека: ")
+    person = [a, b, c, d, e, f]
+    people = []
+    for item in people:
+        person = [a, b, c, d, e, f]
+    answer = input("Для выхода из цикла введите stop: ")
+    if answer.upper() in ("stop"):
+        break
+
