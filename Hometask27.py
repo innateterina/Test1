097be4b3645000be11a9929b0efa252de86ba76d
innateterina <innacharykova@gmail.com>
2023-02-11 20:21:18 +0000

    task inside

---
+++ Hometask27.py
@@ -0,0 +1,11 @@
+#Создать генератор геометрической прогрессии
+
+def geometric_progression(a, b):
+    current = 1
+    for item in range(b):
+        current *= a
+        yield current
+
+
+for x in geometric_progression(5, 10):
+    print(x)
